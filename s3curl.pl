d7f86640ae1fd74ecb70c1d85abcde732836cc23
ddever <david.dever@emc.com>
2015-06-23 10:02:36 -0400

    Added apollo 4x3 endpoints

--- s3curl.pl
+++ s3curl.pl
@@ -41,45 +41,41 @@ my @endpoints = (
         # local testing hostname / address:
         'localhost',
         '127.0.0.1',
-        
+
         # lab hostnames:
         'dhrm01s1.osaas-lab.rcsops.com',
         'dhrm01s2.osaas-lab.rcsops.com',
         'os.osaas-lab.rcsops.com',
-        
+
         # lab ECR site01 addresses:
         '172.29.26.13', '172.29.26.14',
         # lab ECR site02 addresses:
         '172.29.26.77', '172.29.26.78',
-        
+
         # lab ECS testing sites (ECS 2.0)
         # apollo01
-        '172.29.0.20',
+        '172.29.1.148','172.29.1.212','172.29.2.20',
         # apollo02
-        '172.29.0.84',
+        '172.29.4.20','172.29.4.84','172.29.1.84',
         # apollo03
-        '172.29.0.148',
+        '172.29.0.148','172.29.4.212','172.29.0.20',
         # apollo04
-        '172.29.0.212',
+        '172.29.0.212','172.29.2.84','172.29.0.84',
         # beta01
         '172.29.2.148',
         # beta02
         '172.29.2.212',
-        # cronus01
-        '172.29.1.148', '172.29.1.212', '172.29.2.20', '172.29.2.84',
-        # cronus02
-        '172.29.4.20', '172.29.4.84', '172.29.4.148', '172.29.4.212',
         # viva01
         '172.29.3.148',
         # viva02
         '172.29.3.212',
-        
+
         # lab ECS testing sites (ViPR 2.1)
         # feta01
         '172.29.3.20',
         # feta02
         '172.29.3.84',
-        
+
         # gouda beta hostname:
         'storage.emcrubicon.com',
         # gouda vca hostnames:
@@ -89,10 +85,10 @@ my @endpoints = (
         'os.vca.vmware.com',
         # gouda prod hostname:
         'objectstorage.emcrubicon.com',
-        
+
         # gouda ECR stng01-beta addresses:
-#       '173.243.62.10', '173.243.62.11', 
-        '173.243.62.14', '173.243.62.15',        
+#       '173.243.62.10', '173.243.62.11',
+        '173.243.62.14', '173.243.62.15',
         # gouda ECR stng01-vca addresses:
 #       '173.243.62.16',
         '173.243.62.17', '173.243.62.18', '173.243.62.19',
@@ -108,13 +104,13 @@ my @endpoints = (
         # gouda ECR lsvg01-prod addresses:
 #       '173.243.48.212',
         '173.243.48.213', '173.243.48.214', '173.243.48.215',
-        
+
         # gouda ECS staging sites (ECS 2.0)
         # stng01
         '172.29.96.11', '172.29.96.19', '172.29.96.27', '172.29.96.35',
         # lsvg01
         '172.29.112.11', '172.29.112.19', '172.29.112.27', '172.29.112.35',
-        
+
         # gouda ECS prod sites (ViPR 2.1)
         # stng01
         '172.29.96.75', '172.29.96.83', '172.29.96.91', '172.29.96.99',
